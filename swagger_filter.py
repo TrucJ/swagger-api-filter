a0c89a84395cb0ccffc46c10773fefb21e89bc87
TrucJ <trungtruc2357@gmail.com>
2024-08-26 21:19:06 +0700

    fix: update ref recursive

--- swagger_filter.py
+++ swagger_filter.py
@@ -12,9 +12,20 @@ def filter_swagger(input_data, filter_data):
     filtered_paths = {}
     for path, methods in api_filter['paths'].items():
         if path in swagger['paths']:
-            filtered_paths[path] = {method: swagger['paths'][path][method] for method in methods if method in swagger['paths'][path]}
+            filtered_paths[path] = {method: swagger['paths'][path][method] for method in swagger['paths'][path] if method in methods}
 
     filtered_definitions = {}
+    
+    def ref(name):
+        obj_name = name.split('/')[-1]
+        obj = swagger['definitions'][obj_name]
+        filtered_definitions[obj_name] = obj
+        if 'properties' in obj:
+            for prop in obj['properties'].values():
+                if '$ref' in prop:
+                    ref(prop['$ref'])
+                if 'items' in prop and '$ref' in prop['items']:
+                    ref(prop['items']['$ref'])
 
     # Filter the relevant objects
     for path, path_item in filtered_paths.items():
@@ -22,14 +33,12 @@ def filter_swagger(input_data, filter_data):
             if 'parameters' in operation:
                 for parameter in operation['parameters']:
                     if 'schema' in parameter and '$ref' in parameter['schema']:
-                        ref = parameter['schema']['$ref'].split('/')[-1]
-                        filtered_definitions[ref] = swagger['definitions'][ref]
+                        ref(parameter['schema']['$ref'])
 
             if 'responses' in operation:
                 for response in operation['responses'].values():
                     if 'schema' in response and '$ref' in response['schema']:
-                        ref = response['schema']['$ref'].split('/')[-1]
-                        filtered_definitions[ref] = swagger['definitions'][ref]
+                        ref(response['schema']['$ref'])
 
     # Create a new Swagger file
     filtered_swagger = deepcopy(swagger)
